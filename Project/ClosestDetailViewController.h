52372ce7bcf4ee8bd7062f2c8a4b5349a64fdea5
Nick <nickworkmani@gmail.com>
2013-04-20 18:43:53 -0400

    got rid of the name plate in the detailed view, instead uses the navbar title

--- Project/ClosestDetailViewController.h
+++ Project/ClosestDetailViewController.h
@@ -8,16 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ClosestDetailViewController : UIViewController{
+@interface ClosestDetailViewController : UIViewController <UIScrollViewDelegate>{
     NSString *building;
     NSString *campus;
     NSString *description1, *description2;
     NSString *donorName1, *donorName2;
     NSString *donorPic1, * donorPic2;
     
+    UIScrollView *scrollView;
+    UIPageControl *pageControl;
+    BOOL pageControlUsed;
+    
     IBOutlet UITextView *donorsDescription;
 }
 
+@property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, retain) IBOutlet UIPageControl *pageControl;
 @property (nonatomic, retain)NSString *building, *campus, *description1, *description2,
           *donorPic1, *donorPic2, *donorName1, *donorName2;
 @end
--- Project/ClosestDetailViewController.m
+++ Project/ClosestDetailViewController.m
@@ -10,7 +10,7 @@
 
 @implementation ClosestDetailViewController
 @synthesize building, campus, donorName1, donorName2, donorPic1, donorPic2, description1,
-            description2;
+            description2, scrollView, pageControl;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,13 +28,20 @@
     UIImage *backgroundImage = [UIImage imageNamed:@"leather-background.png"];
     UIColor *backgroundPattern = [UIColor colorWithPatternImage:backgroundImage];
     [self.view setBackgroundColor: backgroundPattern];
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    
+    scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     scrollView.pagingEnabled = YES;
+    scrollView.showsHorizontalScrollIndicator = NO;
+    //scrollView.showsVerticalScrollIndicator = NO;
+    scrollView.scrollsToTop = NO;
+    scrollView.delegate = self;
+    self.navigationItem.title = donorName1;
+    
     int numberOfViews = 1;
     if ([donorName2 isEqual: @"NULL"]) {
         CGFloat xOrigin = 0 * self.view.frame.size.width;
         UIView *donorView = [[UIView alloc] initWithFrame:CGRectMake(xOrigin, 0, self.view.frame.size.width, 335)];
-        UIScrollView *innerScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 335)];
+        UIScrollView *innerScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
         innerScrollView.scrollEnabled = YES;
         
         UIImageView *dPic;
@@ -53,35 +60,18 @@
         
         dDescription.frame = CGRectMake(dDescription.frame.origin.x, dDescription.frame.origin.y, dDescription.frame.size.width, dDescription.contentSize.height);
         innerScrollView.contentSize = CGSizeMake(320, dPic.frame.size.height +dDescription.frame.size.height);
-        
-        UIImageView *plate = [[UIImageView alloc] initWithFrame:CGRectMake(0, 335, self.view.frame.size.width, 50)];
-        [plate setImage:[UIImage imageNamed:@"plate.png"]];
-        
-        UILabel *donorsName = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, plate.frame.size.width, plate.frame.size.height)];
-        donorsName.text = donorName1;
-        donorsName.adjustsFontSizeToFitWidth=YES;
-        donorsName.backgroundColor = [UIColor clearColor];
-        donorsName.textColor = [UIColor colorWithRed:255/255.0f green:242/255.0f blue:218/255.0f alpha:1.0f];
-        
-        donorsName.textAlignment =  NSTextAlignmentCenter;
-        [donorsName setFont:[UIFont fontWithName:@"Gil Sans" size:24]];
-        [plate addSubview:donorsName];
-        [donorView addSubview:plate];
-        
-        UIImage *backgroundImage = [UIImage imageNamed:@"leather-background.png"]; //the background gets set twice for a
-        UIColor *backgroundPattern = [UIColor colorWithPatternImage:backgroundImage];//reason
-        [donorView setBackgroundColor: backgroundPattern];
+       
         [scrollView addSubview:donorView];
     }else{
         numberOfViews = 2;
         for (int i = 0; i < 2; i++) {
-            UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 370, self.view.frame.size.width, 20)];
+            pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 15, self.view.frame.size.width, 20)];
             pageControl.numberOfPages = 2;
             pageControl.currentPage = i;
             pageControl.pageIndicatorTintColor = [UIColor darkGrayColor];
             CGFloat xOrigin = i * self.view.frame.size.width;
             UIView *donorView = [[UIView alloc] initWithFrame:CGRectMake(xOrigin, 0, self.view.frame.size.width, 325)];
-            UIScrollView *innerScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 325)];
+            UIScrollView *innerScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 20)];
             
             UIImageView *dPic;
             if (i == 0)
@@ -102,24 +92,7 @@
             [donorView addSubview:innerScrollView];
             
             dDescription.frame = CGRectMake(dDescription.frame.origin.x, dDescription.frame.origin.y, dDescription.frame.size.width, dDescription.contentSize.height);
-            innerScrollView.contentSize = CGSizeMake(300, dPic.frame.size.height +dDescription.frame.size.height);
-            
-            UIImageView *plate = [[UIImageView alloc] initWithFrame:CGRectMake(0, 325, self.view.frame.size.width, 50)];
-            [plate setImage:[UIImage imageNamed:@"plate.png"]];
-            
-            UILabel *donorsName = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, plate.frame.size.width, plate.frame.size.height)];
-            if (i == 0)
-                donorsName.text = donorName1;
-            else
-                donorsName.text = donorName2;
-            donorsName.adjustsFontSizeToFitWidth=YES;
-            donorsName.backgroundColor = [UIColor clearColor];
-            donorsName.textColor = [UIColor colorWithRed:255/255.0f green:242/255.0f blue:218/255.0f alpha:1.0f];
-                     
-            donorsName.textAlignment =  NSTextAlignmentCenter;
-            [donorsName setFont:[UIFont fontWithName:@"Gil Sans" size:24]];
-            [plate addSubview:donorsName];
-            [donorView addSubview:plate];
+            innerScrollView.contentSize = CGSizeMake(self.view.frame.size.width, dPic.frame.size.height +dDescription.frame.size.height);
             
             UIImage *backgroundImage = [UIImage imageNamed:@"leather-background.png"]; //the background gets set twice for a
             UIColor *backgroundPattern = [UIColor colorWithPatternImage:backgroundImage];//reason
@@ -130,29 +103,6 @@
     }
     scrollView.contentSize = CGSizeMake(self.view.frame.size.width * numberOfViews, self.view.frame.size.height);
     [self.view addSubview:scrollView];
-    
-//    self.navigationItem.title = building;
-//    donorsName.text = donorName1;
-//    donorsName.adjustsFontSizeToFitWidth=YES;
-//    
-//    UIImageView *dPic = [[UIImageView alloc] initWithImage:[UIImage imageNamed:donorPic1]];
-//    UITextView *dDescription = [[UITextView alloc] initWithFrame:CGRectMake(0, dPic.frame.size.height, 320, 50)];
-//    dDescription.editable = NO;
-//    dDescription.backgroundColor = [UIColor clearColor];
-//    [dDescription setFont:[UIFont fontWithName:@"Georgia" size:14]];
-//    
-//    dDescription.text = description1;
-//    
-//    [scrollview addSubview:dPic];
-//    [scrollview addSubview:dDescription];
-//    
-//    
-//    dDescription.frame = CGRectMake(dDescription.frame.origin.x, dDescription.frame.origin.y, dDescription.frame.size.width, dDescription.contentSize.height);
-//    scrollview.contentSize = CGSizeMake(320, dPic.frame.size.height +dDescription.frame.size.height);
-//    
-//    UIImage *backgroundImage = [UIImage imageNamed:@"leather-background.png"];
-//    UIColor *backgroundPattern = [UIColor colorWithPatternImage:backgroundImage];
-//    [self.view setBackgroundColor: backgroundPattern];
 }
 
 - (void)didReceiveMemoryWarning
@@ -165,6 +115,42 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+// At the begin of scroll dragging, reset the boolean used when scrolls originate from the UIPageControl
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    pageControlUsed = NO;
+}
+
+// At the end of scroll animation, reset the boolean used when scrolls originate from the UIPageControl
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    pageControlUsed = NO;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)sender
+{
+    // We don't want a "feedback loop" between the UIPageControl and the scroll delegate in
+    // which a scroll event generated from the user hitting the page control triggers updates from
+    // the delegate method. We use a boolean to disable the delegate logic when the page control is used.
+    if (pageControlUsed)
+    {
+        // do nothing - the scroll was initiated from the page control, not the user dragging
+        return;
+    }
+	
+    // Switch the indicator when more than 50% of the previous/next page is visible
+    CGFloat pageWidth = scrollView.frame.size.width;
+    int page = floor((scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    pageControl.currentPage = page;
+    
+    if (page == 0)
+        self.navigationItem.title = donorName1;
+    else
+        self.navigationItem.title = donorName2;
+    
 }
 
 @end
